017bf51fe0ca9516e520e3fef83fd32d3c194151
VladimirChalov <vladimircalov557@gmail.com>
2025-06-24 18:57:59 +0600

    Update Class1.cs

--- task01/Class1.cs
+++ task01/Class1.cs
@@ -1,6 +1,20 @@
-﻿namespace task01;
+using System;
+namespace task01;
 
-public class Class1
+public static class StringExtensions
 {
+    public static bool IsPalindrome(this string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return true;
 
+        var transformation = string.Concat(s.ToLower().Where(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c)));
+
+        for (int i = 0, j = transformation.Length - 1; i < j; i++, j--)
+        {
+            if (transformation[i] != transformation[j])
+                return false;
+        }
+        return true;
+    }
 }
